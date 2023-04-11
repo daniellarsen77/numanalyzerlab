61cc8ae6b76f58a9205a9fa25f760275557cd0a4
daniellarsen77 <daniellarsen127@gmail.com>
2023-04-11 11:08:27 -0400

    Initial Commit

---
+++ Number Analyzer Lab/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}! Please enter a number between 1 and 100");
+int num = int.Parse( Console.ReadLine() );
+
+if ( num >= 1 && num <= 100)
+{
+
+}
+else
+{
+    Console.WriteLine($"Number is invalid please try again {name}.");
+}
+
+if (num % 2 == 0)
+{
+    if (num >= 2 && num <= 24)
+    {
+        Console.WriteLine($"{num} Is Even and less than 25.");
+    }
+    else if (num >= 26 && num <= 60)
+    {
+        Console.WriteLine($"{num} Is Even and between 26 and 60 inclusive.");
+    }
+    else
+    {
+        Console.WriteLine($"{num} Is Even and greater than 60.");
+    }
+}
+else
+{
+    if (num < 60)
+    {
+        Console.WriteLine($"{num} Is Odd and less than 60.");
+    }
+    else
+    {
+        Console.WriteLine($"{num} Is Odd and greater than 60.");
+    }
+}
+    
